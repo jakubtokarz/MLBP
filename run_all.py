ed3621d2bf5711f1e2eba75f7cbff5efaaf831d5
Kuba Tokarz <j.m.tokarz@student.tudelft.nl>
2022-04-30 02:00:46 +0200

    Add writing to file of results of the run_all.py

--- run_all.py
+++ run_all.py
@@ -4,11 +4,14 @@ from itertools import takewhile
 
 all_MLBP_instances = os.listdir('inst\\mlbp')
 
+t = 1
 opt_count = 0
 fea_count = 0
 idk_count = 0
+result = ""
+
 for inst in all_MLBP_instances:
-    input_str = ".\\x64\\Debug\\MLBP.exe ifile inst\\mlbp\\%s prob MLBP ttime 60" % inst
+    input_str = ".\\x64\\Debug\\MLBP.exe ifile inst\\mlbp\\{} prob MLBP ttime {}".format(inst, t)
 
     try:
         res = subprocess.run(input_str, shell=True, capture_output=True, text=True) #, timeout=1
@@ -30,6 +33,7 @@ for inst in all_MLBP_instances:
             elif status == 'Unknown':
                 idk_count += 1
                 print(inst + " -> " + status)
+                result += inst + " -> " + status + '\n'
                 continue
             else:
                 raise ValueError("Status: " + status + " in " + inst)
@@ -38,8 +42,10 @@ for inst in all_MLBP_instances:
             score = a2[:a2.index('\n')].strip()
 
             print(inst + " -> {}: {}".format(status, score))
+            result += inst + " -> {}: {}".format(status, score) + '\n'
         except ValueError:
             print(output)
+            result += output + '\n'
 
     except subprocess.TimeoutExpired:
         break
@@ -47,3 +53,9 @@ for inst in all_MLBP_instances:
 
 print("Optimal: " + str(opt_count) + " | " + "Feasible: " + str(fea_count) + " | " + "Unknown: " + str(idk_count))
 print(str(opt_count + fea_count) + '/' + str(len(all_MLBP_instances)))
+result += "Optimal: " + str(opt_count) + " | " + "Feasible: " + str(fea_count) + " | " + "Unknown: " + str(idk_count) + '\n'
+result += str(opt_count + fea_count) + '/' + str(len(all_MLBP_instances)) + '\n'
+
+file = open("run_all_res_{}.txt".format(t), "w")
+file.write(result)
+file.close()
