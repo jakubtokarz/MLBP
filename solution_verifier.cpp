c40bb39e94757d54e1af31413baa45ca23e2194a
Kuba Tokarz <j.m.tokarz@student.tudelft.nl>
2022-05-12 23:25:18 +0200

    Fix solution_verifier for MLBP bug

--- solution_verifier.cpp
+++ solution_verifier.cpp
@@ -205,8 +205,11 @@ bool SolutionVerifier<MLBPCC>::verify(const Instance<MLBPCC>& inst, const Soluti
 	for (int k : inst.M) {
 		std::vector<std::vector<int>> same_bins_set;
 
-		for (int i = 0; i < inst.n[0]; i++) {
+		for (int i = 0; i < inst.n[k]; i++) {
 			same_bins_set.push_back(std::vector<int>());
+		}
+
+		for (int i = 0; i < inst.n[0]; i++) {
 			for (int j = 1; j < same_bins_set[i].size(); j++) {
 				//3.
 				if (sol.item_to_bins[k - 1][same_bins_set[i][0]] != sol.item_to_bins[k - 1][same_bins_set[i][j]]) {
